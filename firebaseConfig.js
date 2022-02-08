09b843aa57dbb88f992d788720363f3e939c196b
Philip <philipbdev@gmail.com>
2022-02-08 13:27:25 -0500

    Learning Firebase

---
+++ firebaseConfig.js
@@ -0,0 +1,13 @@
+import { initializeApp } from 'firebase/app';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyAlIMNh0ozyp_74dsch4RGnqghcJ2E1jfs',
+  authDomain: 'friendly-reminders-os.firebaseapp.com',
+  projectId: 'friendly-reminders-os',
+  storageBucket: 'friendly-reminders-os.appspot.com',
+  messagingSenderId: '335716950394',
+  appId: '1:335716950394:web:d57a23eca2ba71222376c3',
+  measurementId: 'G-X71C0N6LNQ',
+};
+
+initializeApp(firebaseConfig);
